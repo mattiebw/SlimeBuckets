1d19f5ba24c76cb695b23e0ae24c02c042bad13e
Matt Ware <mattbware3@gmail.com>
2024-04-02 14:11:54 +0100

    Remove example item

--- common/src/main/java/dev/mattware/slimebuckets/SlimeBuckets.java
+++ common/src/main/java/dev/mattware/slimebuckets/SlimeBuckets.java
@@ -20,7 +20,7 @@ public class SlimeBuckets
 	public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(MOD_ID, Registries.CREATIVE_MODE_TAB);
 	public static final RegistrySupplier<CreativeModeTab> SLIME_BUCKETS_TAB = TABS.register("slime_buckets_tab", () ->
 			CreativeTabRegistry.create(Component.translatable("itemGroup." + MOD_ID + ".slime_buckets_tab"),
-					() -> new ItemStack(SlimeBucketsItems.EXAMPLE_ITEM.get())));
+					() -> new ItemStack(SlimeBucketsItems.SLIME_BUCKET.get())));
 
 	public static void init() {
 		LOGGER.info("Initialising Slime Buckets ^-^");
