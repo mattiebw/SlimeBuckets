5895c024873df30d3c7df70fbc5c1aefed9a14ad
Matt Ware <mattbware3@gmail.com>
2024-04-03 06:44:11 +0100

    Picking up the slime now creates a slime bucket

--- common/src/main/java/dev/mattware/slimebuckets/mixin/SlimeMixin.java
+++ common/src/main/java/dev/mattware/slimebuckets/mixin/SlimeMixin.java
@@ -1,7 +1,10 @@
 package dev.mattware.slimebuckets.mixin;
 
 import dev.mattware.slimebuckets.SlimeBuckets;
+import dev.mattware.slimebuckets.item.SlimeBucketsItems;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -9,6 +12,7 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.monster.Slime;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemUtils;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
@@ -20,20 +24,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class SlimeMixin {
     @Inject(method = "mobInteract", at = @At("HEAD"), cancellable = true)
     public void onInteract(Player player, InteractionHand interactionHand, CallbackInfoReturnable<InteractionResult> cir) {
-        ItemStack item = player.getItemInHand(interactionHand);
-        if (item.getItem() == Items.BUCKET &&
+        ItemStack heldItem = player.getItemInHand(interactionHand);
+        if (heldItem.getItem() == Items.BUCKET &&
                 (Mob)((Object)this) instanceof Slime slime)
         {
             Level level = slime.level();
             if (slime.getSize() == 1) { // Can only pick up the smallest slimes
                 SlimeBuckets.LOGGER.info("Slime should be picked up now");
                 slime.playSound(SoundEvents.BUCKET_FILL_FISH, 1.0f, 1.0f);
+                ItemStack bucket = new ItemStack(SlimeBucketsItems.SLIME_BUCKET.get());
+                if (!level.isClientSide) {
+                    CriteriaTriggers.FILLED_BUCKET.trigger((ServerPlayer)player, bucket);
+                }
+                ItemStack newHeldResult = ItemUtils.createFilledResult(heldItem, player, bucket, false);
+                player.setItemInHand(interactionHand, newHeldResult);
                 slime.discard();
                 cir.setReturnValue(InteractionResult.sidedSuccess(level.isClientSide));
             } else {
                 if (!level.isClientSide)
                     player.displayClientMessage(Component.literal("THIS SLIME IS TOO FUCKING FAT"), true);
-                SlimeBuckets.LOGGER.info("Slime is too big :(");
             }
         }
     }
