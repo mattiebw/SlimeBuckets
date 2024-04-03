86412542da504d9ac1e0bf719e86e96e1fffb802
Matt Ware <mattbware3@gmail.com>
2024-04-03 08:10:45 +0100

    Slime bucket can place slime

--- common/src/main/java/dev/mattware/slimebuckets/item/SlimeBucketItem.java
+++ common/src/main/java/dev/mattware/slimebuckets/item/SlimeBucketItem.java
@@ -2,11 +2,27 @@ package dev.mattware.slimebuckets.item;
 
 import dev.mattware.slimebuckets.SlimeBuckets;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.monster.Slime;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -14,6 +30,38 @@ import java.util.List;
 public class SlimeBucketItem extends Item {
     public SlimeBucketItem() {
         super(new Item.Properties().arch$tab(SlimeBuckets.SLIME_BUCKETS_TAB).stacksTo(1));
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
+        ItemStack itemStack = player.getItemInHand(interactionHand);
+        BlockHitResult blockHitResult = getPlayerPOVHitResult(level, player, ClipContext.Fluid.SOURCE_ONLY);
+        if (blockHitResult.getType() == HitResult.Type.MISS) {
+            return InteractionResultHolder.pass(itemStack);
+        } else if (blockHitResult.getType() != HitResult.Type.BLOCK) {
+            return InteractionResultHolder.pass(itemStack);
+        } else {
+            BlockPos blockPos = blockHitResult.getBlockPos();
+            Direction direction = blockHitResult.getDirection();
+            BlockPos blockPos2 = blockPos.relative(direction);
+            if (level.mayInteract(player, blockPos) && player.mayUseItemAt(blockPos2, direction, itemStack)) {
+                player.awardStat(Stats.ITEM_USED.get(this));
+                level.playSound(player, blockPos2, SoundEvents.BUCKET_EMPTY_FISH, SoundSource.NEUTRAL, 1.0f, 1.0f);
+                if (level instanceof ServerLevel serverLevel)
+                {
+                    Slime slime = (Slime) EntityType.SLIME.create(serverLevel);
+                    if (slime != null)
+                    {
+                        slime.setSize(1, true);
+                        slime.moveTo(blockPos2, 0, 0);
+                        serverLevel.addFreshEntity(slime);
+                    }
+                }
+                return InteractionResultHolder.sidedSuccess(!player.getAbilities().instabuild ? new ItemStack(Items.BUCKET) : itemStack, level.isClientSide);
+            } else {
+                return InteractionResultHolder.fail(itemStack);
+            }
+        }
     }
 
     @Override
