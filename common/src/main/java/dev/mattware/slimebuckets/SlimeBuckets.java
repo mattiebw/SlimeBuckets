39210c4a79ba3c4598cda8bf7b0836fdd247110e
Matt Ware <mattbware3@gmail.com>
2024-04-04 13:25:56 +0100

    Config menu now works on forge

--- common/src/main/java/dev/mattware/slimebuckets/SlimeBuckets.java
+++ common/src/main/java/dev/mattware/slimebuckets/SlimeBuckets.java
@@ -24,7 +24,6 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.CreativeModeTab;
--- forge/src/main/java/dev/mattware/slimebuckets/forge/SlimeBucketsForge.java
+++ forge/src/main/java/dev/mattware/slimebuckets/forge/SlimeBucketsForge.java
@@ -3,8 +3,13 @@ package dev.mattware.slimebuckets.forge;
 import com.google.common.eventbus.EventBus;
 import dev.architectury.platform.forge.EventBuses;
 import dev.mattware.slimebuckets.SlimeBuckets;
+import dev.mattware.slimebuckets.config.SlimeBucketsConfig;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -14,6 +19,13 @@ public class SlimeBucketsForge {
     public SlimeBucketsForge() {
 		// Submit our event bus to let architectury register our content on the right time
         EventBuses.registerModEventBus(SlimeBuckets.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+
+        // Register config GUI with forge
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> () -> ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
+                () -> new ConfigScreenHandler.ConfigScreenFactory(
+                        (client, parent) -> AutoConfig.getConfigScreen(SlimeBucketsConfig.class, parent).get()
+                )));
+
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
         SlimeBuckets.init();
     }
